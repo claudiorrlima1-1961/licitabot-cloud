722abb1806d12100cd4183b5cdaef0948c5126a7
claudiorrlima1-1961 <claudiorrlima1@gmail.com>
2025-10-21 23:39:16 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import (
-FastAPI, Request, UploadFile, File, Header,
-HTTPException,
+FastAPI, Request, UploadFile, File, Header,HTTPException
+
 )
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
